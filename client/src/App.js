23e02b905a53d65bb2547350520af0869e9f20d8
paolocaminiti <paolo.caminiti@gmail.com>
2022-01-05 18:33:54 +0000

    App calls prints service

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,39 @@
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
+
+const PRINTS_API_ENTRYPOINT = 'http://localhost:3001/prints'
+
+function Next ({ onClick }) {
+  return (
+    <button onClick={onClick}>load more</button>
+  )
+}
+
+function Print ({ title, dated, url }) {
+  return (
+    <img key={url} src={url} width="256"/>
+  )
+}
 
 function Gallery () {
-  const { page, setPage } = useState(1)
+  const [page, setPage] = useState(1)
+  const [isLastPage, setIsLastPage] = useState(false)
+  const [records, setRecords] = useState(null)
+
+  useEffect(async () => {
+    const { records, isLastPage } = await (await fetch(`${PRINTS_API_ENTRYPOINT}?page=${page}`)).json()
+    setIsLastPage(isLastPage)
+    setRecords(records)
+  }, [page])
+
   return (
-    <div>{page}</div>
+    <div>
+      <div>
+        Gallery page {page}
+        {!isLastPage && <Next onClick={() => setPage(page + 1)} />}
+      </div>
+      {records && records.map(Print)}
+    </div>
   )
 }
 
@@ -12,7 +42,7 @@ function App() {
     <div className="App">
       <Gallery />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
