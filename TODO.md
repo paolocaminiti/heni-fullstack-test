faa35633d388b3ecdc39c2c2c5567fc6235f84f5
paolocaminiti <paolo.caminiti@gmail.com>
2022-01-06 21:45:55 +0000

    Client sfx

--- TODO.md
+++ TODO.md
@@ -1,4 +1,3 @@
-2. Provide a basic react driven front end for the feed that displays the image.
 3. Provide information on the detail of print you feel may be relevant (title etc).
 4. Commit your code for the server and the client to GitHub and provide us with the url to the repository.
 5. Add any tests you feel are required, where necessary.
--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import './App.css'
 import { World, Item } from 'react-dom-box2d'
 
 const PRINTS_API_ENTRYPOINT = 'http://localhost:3001/prints'
+const PRINT_SIDE_PX = 256
+const MESSAGE_WELLCOME = 'Destroy all Prints!'
+const MESSAGE_ENDGAME = 'You Won! Cultural heritage has been erased from the world!'
 
 async function fetchPrints (page) {
   return await (await fetch(`${PRINTS_API_ENTRYPOINT}?page=${page}`)).json()
@@ -12,17 +15,22 @@ function getDimensions () {
   return { x: window.innerWidth, y: window.innerHeight }
 }
 
-function getRandomInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+function getSpawnPoint(dimensions) {
+  const min = dimensions.x / 5
+  const max = dimensions.x / 5 * 4
+  return {
+    x: PRINT_SIDE_PX,
+    y: Math.floor(Math.random() * (max - min + 1) ) + min,
+  }
 }
 
-const PRINT_SIDE_PX = 256
 
 function Gallery () {
   const [page, setPage] = useState(1)
   const [dimensions, setDimensions] = useState(getDimensions())
   const [isLastPage, setIsLastPage] = useState(false)
   const [prints, setPrints] = useState([])
+  const [title, setTitle] = useState(MESSAGE_WELLCOME)
   const flameRef = useRef()
 
   useEffect(() => {
@@ -35,11 +43,16 @@ function Gallery () {
   }, [page])
 
   const destroyPrint = useCallback(url => {
+    const sfx = window.firesfx.cloneNode(true)
+    sfx.play()
+    const target = prints.find(i => i.url === url)
+    const nextTitle = `${target.title} from ${target.dated} destroyed!`
+    setTitle(nextTitle)
     const nextPrints = prints.filter(i => i.url !== url)
     setPrints(nextPrints)
     if (nextPrints.length === 0) {
       if (isLastPage) {
-        alert('You Won! Cultural heritage has been erased from the world!')
+        setTitle(MESSAGE_ENDGAME)
       } else {
         setPage(page + 1)
       }
@@ -61,29 +74,35 @@ function Gallery () {
 
   return (
     <div>
-      <img ref={flameRef} className="flame" src="https://www.freeiconspng.com/uploads/flame-png-flame-28.png" width="50" />
+      <audio id="firesfx" preload="auto">
+        <source src="./fire.mp3" type="audio/mpeg" />
+      </audio>
+      <img ref={flameRef} className="flame" src="./flame.png" width="50" />
       <div className="osd">
-        <div>Destroy all Prints!</div>
-        <div>Level: {page}</div>
+        <div>{title}</div>
+        <div className="level">Level {page}</div>
       </div>
       {prints && <World
         height={dimensions.y}
         width={dimensions.x}
         className="world"
       >
-        {prints.map(({ url }) => (
-          <Item
-            key={url}
-            restitution={0.5}
-            left={getRandomInteger(dimensions.x / 5, dimensions.x / 5 * 3)}
-            top={PRINT_SIDE_PX}
-            height={PRINT_SIDE_PX}
-            width={PRINT_SIDE_PX}
-            shape="box"
-          >
-            <img src={url} onClick={e => destroyPrint(url)} />
-          </Item>
-        ))}
+        {prints.map(({ url }) => {
+          const { x, y } = getSpawnPoint(dimensions)
+          return (
+            <Item
+              key={url}
+              restitution={0.5}
+              top={x}
+              left={y}
+              height={PRINT_SIDE_PX}
+              width={PRINT_SIDE_PX}
+              shape="box"
+            >
+              <img src={url} onClick={e => destroyPrint(url)} />
+            </Item>
+          )
+        })}
       </World>}
     </div>
   )
@@ -91,9 +110,7 @@ function Gallery () {
 
 function App() {
   return (
-    <div className="App">
-      <Gallery />
-    </div>
+    <Gallery />
   )
 }
 
